f340cf1fdf622af06c85059ce628c2588fc8fb77
Danya Polyakov <gerk@MacBook-Air-Danya.local>
2025-01-16 19:00:00 +0300

    Fixing headers and cors

--- backend/pkg/handler/middleware.go
+++ backend/pkg/handler/middleware.go
@@ -9,9 +9,9 @@ import (
 )
 
 const (
-	AuthorizationHeader = "authorization"
+	AuthorizationHeader = "Authorization"
 	userIdCtx           = "id"
-	userAccessCtx       = "access"
+	userAccessCtx       = "Access"
 )
 
 func (h *Handler) UserIdentity(c *gin.Context) {
@@ -31,7 +31,7 @@ func (h *Handler) UserIdentity(c *gin.Context) {
 	expTime, id, access, err := h.services.Authorization.Parse(headerParts[1])
 
 	if err != nil {
-		NewErrorResponse(c, http.StatusBadRequest, gotype.ErrAccessToken)
+		NewErrorResponse(c, http.StatusUnauthorized, gotype.ErrUnauthorized)
 		return
 	}
 
